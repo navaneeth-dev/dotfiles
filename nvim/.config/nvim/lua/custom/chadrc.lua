6fbde21d2cebb41a072f5b38c37ed30d7cdbbbe7
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2023-06-20 13:19:48 +0000

    added language servers

--- nvim/.config/nvim/lua/custom/chadrc.lua
+++ nvim/.config/nvim/lua/custom/chadrc.lua
@@ -1,5 +1,5 @@
----@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'onedark'}
- M.plugins = 'custom.plugins'
- return M
+---@type ChadrcConfig
+local M = {}
+M.ui = { theme = "onedark" }
+M.plugins = "custom.plugins"
+return M
--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -1,33 +1,35 @@
 local plugins = {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      ensure_installed = {"typescript", "lua", "prisma", "svelte", "tsx", "json", "vim", "html", "css"},
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-       "jose-elias-alvarez/null-ls.nvim",
-       config = function()
-         require "custom.configs.null-ls"
-       end,
-     },
-     config = function()
-        require "plugins.configs.lspconfig"
-        require "custom.configs.lspconfig"
-     end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "html-lsp",
-        "prettier",
-        "stylua",
-      },
-    },
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = { "typescript", "lua", "prisma", "svelte", "tsx", "json", "vim", "html", "css" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.configs.null-ls")
+			end,
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"lua-language-server",
+				"html-lsp",
+				"prettier",
+				"stylua",
+        "tailwindcss",
+        "typescript-language-server"
+			},
+		},
+	},
 }
 return plugins
