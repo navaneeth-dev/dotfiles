5797ebcd113f2b4946531938cec0bf8f6de6595f
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2023-06-20 06:30:00 +0000

    added nvim

--- install.sh
+++ install.sh
@@ -39,4 +39,11 @@ curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install
 
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+
 stow */ --target ~
