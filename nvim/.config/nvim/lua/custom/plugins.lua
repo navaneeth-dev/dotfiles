ebbedefdf9f8bb075e7c0be397667e2bb25e5740
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2023-06-20 12:52:01 +0000

    added null-ls and proper plugins

--- nvim/.config/nvim/lua/custom/plugins.lua
+++ nvim/.config/nvim/lua/custom/plugins.lua
@@ -4,22 +4,30 @@ local plugins = {
     opts = {
       ensure_installed = {"typescript", "lua", "prisma", "svelte", "tsx", "json", "vim", "html", "css"},
     },
+  },
+  {
     "neovim/nvim-lspconfig",
+    dependencies = {
+       "jose-elias-alvarez/null-ls.nvim",
+       config = function()
+         require "custom.configs.null-ls"
+       end,
+     },
      config = function()
         require "plugins.configs.lspconfig"
         require "custom.configs.lspconfig"
      end,
-     {
-   "williamboman/mason.nvim",
-     opts = {
-        ensure_installed = {
-          "lua-language-server",
-          "html-lsp",
-          "prettier",
-          "stylua",
-        },
-      },
-    }
   },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "stylua",
+      },
+    },
+  }
 }
 return plugins
