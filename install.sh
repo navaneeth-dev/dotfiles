f3fde169636f2ea80e2a6d19618d2caac7d0fc6d
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2023-06-20 05:45:49 +0000

    ripgrep

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo apt-get install -y bat
 sudo apt-get install -y unzip
 sudo apt-get install -y wget
 sudo apt-get install -y tmux
+sudo apt-get install -y ripgrep
 
 sudo apt-get install -y stow
 
