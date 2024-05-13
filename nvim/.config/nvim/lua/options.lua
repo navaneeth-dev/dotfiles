90cad5d555ad5b95106f6100d3a619eaf10d06fe
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-05-13 11:50:34 -0400

    feat: initial structure

---
+++ nvim/.config/nvim/lua/options.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
---
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+
+			harpoon:setup()
+
+			vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+		end,
+	},
+}
