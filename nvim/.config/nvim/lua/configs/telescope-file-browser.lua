3abc01ef9cce8b3580ca0f63ad0afbc8744b3fce
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-05-13 10:45:24 -0400

    refactor!: removed nvchad

--- nvim/.config/nvim/lua/configs/telescope-file-browser.lua
+++
@@ -1,3 +0,0 @@
-require("telescope").load_extension "file_browser"
-
-vim.keymap.set("n", "<space>pv", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
--- nvim/.config/nvim/lua/options.lua
+++
@@ -1,7 +0,0 @@
-require "nvchad.options"
-
-local o = vim.o
-o.cursorlineopt = "both" -- to enable cursorline!
-o.relativenumber = true
-o.number = true
-o.colorcolumn = "80"
