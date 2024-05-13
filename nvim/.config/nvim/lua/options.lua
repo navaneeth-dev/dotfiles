947c9c4a505b52107299fdf1913b6b2a5227fe3b
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-05-13 00:17:51 -0400

    feat: colorcolumn

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,8 +1,7 @@
 require "nvchad.options"
 
--- add yours here!
-
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = "both" -- to enable cursorline!
 o.relativenumber = true
 o.number = true
+o.colorcolumn = "80"
