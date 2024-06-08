9792a6284ec1d8a84e030021939dfe5955271516
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-06-08 18:35:54 +0530

    feat: fugitive keybind

--- roles/nvim/files/lua/plugins/fugitive.lua
+++ roles/nvim/files/lua/plugins/fugitive.lua
@@ -1,5 +1,8 @@
 return {
     {
-        "tpope/vim-fugitive"
+        "tpope/vim-fugitive",
+        config = function ()
+            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        end
     }
 }
