fbd71dec7a94c34280ac023dafd46ca32076eaf0
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-06-17 10:53:49 +0530

    fix: horizontal scroll telescope and help tags

--- roles/nvim/files/lua/plugins/telescope.lua
+++ roles/nvim/files/lua/plugins/telescope.lua
@@ -1,12 +1,14 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		'nvim-telescope/telescope.nvim', commit = 'c392f1b78eaaf870ca584bd698e78076ed301b26',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 			vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		end
-	},
+			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        end
+    },
 }
