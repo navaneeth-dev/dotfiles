27ba84f352bf46e5f09299e93f1f2ec32936e5ca
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-06-21 18:36:33 +0530

    fix: remove ctrl-p and back to leaderpv

--- roles/nvim/files/lua/plugins/oil.lua
+++ roles/nvim/files/lua/plugins/oil.lua
@@ -2,9 +2,13 @@ return {
     'stevearc/oil.nvim',
     dependencies = { "nvim-tree/nvim-web-devicons" },
     event = { "VimEnter */*,.*", "BufNew */*,.*" },
-    opts = {},
+    opts = {
+        keymaps = {
+            ["<C-p>"] = false,
+        },
+    },
     -- lazy = false,
     keys = {
-        { "-", "<CMD>Oil<CR>", mode = "n", desc = "Open Oil" }
+        { "<leader>pv", "<CMD>Oil<CR>", mode = "n", desc = "Open Oil" }
     },
 }
