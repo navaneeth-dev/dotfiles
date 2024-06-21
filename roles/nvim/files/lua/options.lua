e1166d760a6ff224d5a7e79255a93de5d1531ec1
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-06-21 18:30:26 +0530

    feat: nvim undotree

--- roles/nvim/files/lua/options.lua
+++ roles/nvim/files/lua/options.lua
@@ -18,7 +18,8 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undofile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
