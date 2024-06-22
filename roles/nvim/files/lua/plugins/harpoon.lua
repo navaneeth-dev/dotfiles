02efa6b2413c9bb6873d39b259b9faeed3273c0d
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-06-22 21:02:12 +0530

    fix: quicklist conflicts

--- roles/nvim/files/lua/plugins/harpoon.lua
+++ roles/nvim/files/lua/plugins/harpoon.lua
@@ -11,10 +11,11 @@ return {
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 			vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+            for _, idx in ipairs { 1, 2, 3, 4, 5 } do
+                vim.keymap.set("n", string.format("<space>%d", idx), function()
+                    harpoon:list():select(idx)
+                end)
+            end
 
             -- Toggle previous & next buffers stored within Harpoon list
             vim.keymap.set("n", "<C-S-J>", function() harpoon:list():prev() end)
