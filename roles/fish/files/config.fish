ee060efd2f664a0f3908f7f047c9fc4c08d932cc
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-06-23 08:35:31 +0530

    feat: gpg signing keys for git

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -2,6 +2,7 @@ set PATH $PATH ~/.local/bin
 set PATH $PATH ~/go/bin
 set PATH $PATH /opt/android-studio/bin
 
+set GPG_TTY $(tty)
 set SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
 
 if status is-interactive
