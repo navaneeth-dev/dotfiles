0fcc5d6a627ca46d41503a90b54a69443b35e850
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-07-19 20:15:41 +0530

    feat: use gnome keyring ssh keys and KUBECONFIG

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -2,8 +2,12 @@ set PATH $PATH ~/.local/bin
 set PATH $PATH ~/go/bin
 set PATH $PATH /opt/android-studio/bin
 
-set GPG_TTY $(tty)
-set SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+set -x GPG_TTY $(tty)
+set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gcr/ssh"
+set -gx PATH $PATH $HOME/.krew/bin
+
+export KUBECONFIG=$HOME/.kube/proxyconfig:$HOME/.kube/ociconfig:$HOME/.kube/config
+#export OCI_CLI_AUTH=security_token
 
 if status is-interactive
 end
