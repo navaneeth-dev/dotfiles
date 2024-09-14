38733231bbbe4b5398dfe947e33e0c8b29ed2259
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-09-14 18:17:50 +0530

    feat: nvim lspconfig

--- roles/nvim/files/lua/plugins/lsp.lua
+++ roles/nvim/files/lua/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
