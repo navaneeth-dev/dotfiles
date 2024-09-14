e7043748837a913c553f6a05e7fbb3a69a2905d4
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-09-14 18:22:43 +0530

    refactor: formatting

--- roles/nvim/files/lua/plugins/treesitter.lua
+++ roles/nvim/files/lua/plugins/treesitter.lua
@@ -2,14 +2,14 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function () 
+        config = function ()
             local configs = require("nvim-treesitter.configs")
 
             configs.setup({
                 ensure_installed = { "lua" },
                 sync_install = false,
                 highlight = { enable = true },
-                indent = { enable = true },  
+                indent = { enable = true },
             })
         end
     }
