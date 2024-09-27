4a28ceb2bc5ef20f6c11ef08e2f47f6d24bc6d30
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-09-27 15:48:26 +0530

    feat: telescope find all files

--- roles/nvim/files/lua/plugins/telescope.lua
+++ roles/nvim/files/lua/plugins/telescope.lua
@@ -1,21 +1,22 @@
 return {
-	{
-		'nvim-telescope/telescope.nvim', commit = 'c392f1b78eaaf870ca584bd698e78076ed301b26',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fa', function ()
-                builtin.find_files({ prompt_prefix = "🔍", find_command = { "rg", "--ignore", "--hidden", "--files" } })
+    {
+        'nvim-telescope/telescope.nvim',
+        commit = 'c392f1b78eaaf870ca584bd698e78076ed301b26',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fa', function()
+                builtin.find_files({ prompt_prefix = "🔍", find_command = { "rg", "--no-ignore", "--hidden", "--files" } })
             end, {})
-			vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-			vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-			vim.keymap.set('n', '<leader>fj', builtin.jumplist, {})
-			vim.keymap.set('n', '<leader>fm', builtin.marks, {})
-			vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader>fj', builtin.jumplist, {})
+            vim.keymap.set('n', '<leader>fm', builtin.marks, {})
+            vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
         end
     },
 }
